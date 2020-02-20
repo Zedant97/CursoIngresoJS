db8475194fef3a0428c8c0756207c0726d1c3a5a
zedant97 <perezalejandro266@gmail.com>
2020-02-20 06:26:51 -0300

    hola

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,25 +5,60 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var eleccionparseado;
+var tijera;
+
 
 function comenzar()
 {
+    eleccionMaquina = Math.random() * 3 + 1;
+    eleccionparseado = parseInt(eleccionMaquina);
 	
 
 
 }//FIN DE LA FUNCIÓN
-function piedra()
+
+function piedra ()
 {
-	
+    var piedra = 1
+    if (piedra < eleccionparseado) {
+        alert("perdiste!");
+    } else {
+        if (piedra > eleccionparseado) {
+            alert("Ganaste!");
+        } else {
+            alert("empataste!");
+        }
+    }
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
+    var papel = 2
+    if (papel < eleccionparseado) {
+        alert("perdiste!");
+    } else {
+        if (papel > eleccionparseado) {
+            alert("ganaste!");
+        } else {
+            alert("empataste!");
+        }
+    }
 
 }//FIN DE LA FUNCIÓN
+
 function tijera()
 {
-	
+    var tijera = 3;
+    if (tijera >= eleccionparseado) {
+        alert("perdiste!");
+    } else {
+        if (tijera > eleccionparseado) {
+            alert("ganaste!");
+        } else {
+            alert("empataste!");
+        }
+    }
+
 
 }//FIN DE LA FUNCIÓN
